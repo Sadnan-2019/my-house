b11b0d4e0ceb1deb9096b79aa32794632e31e9c9
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-11-14 00:39:36 +0600

    side nav

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,3 +1,10 @@
+import {
+  Button,
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+} from "@material-tailwind/react";
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
@@ -7,29 +14,45 @@ const DashBoard = () => {
       <div class="drawer drawer-mobile   ">
         <input id="my-drawer" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content  ">
-           
           <Outlet></Outlet>
         </div>
         <div class="drawer-side  ">
           <label for="my-drawer" class="drawer-overlay"></label>
           <ul class="menu text-black shadow-lg p-4  overflow-y-auto w-60 bg-white  ">
             <div className="flex justify-items-center items-center">
-              <img alt=""
-                className="w-4"
-                src="https://cdn-icons-png.flaticon.com/128/2893/2893521.png"
-              />
+              
               <li class=" ">
                 <Link to="/dashboard">Dashboard</Link>
               </li>
             </div>
             <div className="flex justify-items-center items-center">
-              <img alt=""
+              <img
+                alt=""
                 className="w-4"
                 src="https://cdn-icons-png.flaticon.com/512/863/863823.png"
               />
-              <li className=" ">
-                <Link to="/dashboard/landlord">Add Landlord</Link>
-              </li>
+              <Menu
+                animate={{
+                  mount: { y: 0 },
+                  unmount: { y: 25 },
+                }}
+                className="bg-white"
+              >
+                <MenuHandler>
+                  <Button className="bg-white shadow-none text-black shadow-0">
+                    Land Lord
+                  </Button>
+                </MenuHandler>
+                <MenuList>
+                  <MenuItem>
+                    <li className=" hover:shadow-none">
+                      <Link to="/dashboard/landlord">Add Landlord</Link>
+                    </li>
+                  </MenuItem>
+                  <MenuItem>Manage Lanlord</MenuItem>
+                  {/* <MenuItem>Menu Item 3</MenuItem> */}
+                </MenuList>
+              </Menu>
             </div>
             <li>
               <Link to="/dashboard/review">My review</Link>
@@ -40,7 +63,6 @@ const DashBoard = () => {
             </li>
           </ul>
         </div>
-       
       </div>
     </div>
   );
